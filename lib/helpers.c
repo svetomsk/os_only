6433a7a6266e8552d6263d378d792560ab4fc074
svetomsk <svt2404@gmail.com>
2015-03-08 00:53:24 +0300

    Fucntions signature and makefile for windows and linux added

---
+++ lib/helpers.c
@@ -0,0 +1,13 @@
+#include "helpers.h"
+
+ssize_t read_(int fd, void *buf, size_t count) {
+	return 0;
+}
+
+ssize_t write_(int fd, void *buf, size_t count) {
+	return 0;
+}
+
+int main() {
+	return 0;
+}
---
+++ lib/helpers.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+ssize_t read_(int fd, void *buf, size_t count);
+ssize_t write_(int fd, void *buf, size_t count);
