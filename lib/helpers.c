fdefaa10757cdbbefc9d2246a8655dcc1b965ca7
svetomsk <svt2404@gmail.com>
2015-03-08 09:07:30 +0300

    Function read_ first assumption

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,7 +1,13 @@
 #include "helpers.h"
 
 ssize_t read_(int fd, void *buf, size_t count) {
-	return 0;
+	char *buffer = buf;
+	int c;
+	int i = 0;
+	while((c = getchar()) != '\n' && i < count) {
+		buffer[i] = c;
+	}
+	return i;
 }
 
 ssize_t write_(int fd, void *buf, size_t count) {
