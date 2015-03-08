24683cfbb94b4bcd857edb1ebd381ed9175e12eb
svetomsk <svt2404@gmail.com>
2015-03-08 10:47:27 +0300

    Handling error from getchar() and putchar() functions

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,6 +1,7 @@
 #include "helpers.h"
 
 ssize_t read_(int fd, void *buf, size_t count) {
+	/* EBADF error */
 	if(fd != STDIN_FILENO) {
 		errno = EBADF;
 		return -1;
@@ -14,10 +15,16 @@ ssize_t read_(int fd, void *buf, size_t count) {
 		i++;
 	}
 
+	/* Handling error from getchar(). errno set by getchar() */
+	if(ferror(stdin)) {
+		return -1;
+	}
+
 	return i;
 }
 
 ssize_t write_(int fd, void *buf, size_t count) {
+	/* EBADF error */
 	if(fd != STDOUT_FILENO) {
 		errno = EBADF;
 		return -1;
@@ -25,12 +32,27 @@ ssize_t write_(int fd, void *buf, size_t count) {
 
 	const char *buffer = buf;
 	ssize_t i = 0;
+
 	while(i < count) {
 		putchar(buffer[i++]);
 	}
+
+	/* Handling error from putchar(). errno set by putchar() */
+	if(ferror(stdout)) {
+		return -1;
+	}
+
 	return i;
 }
 
 int main() {
+	char buf[3];
+	int r = read_(STDIN_FILENO, buf, 3);
+	if(r == -1)
+		perror(strerror(errno));
+
+	int w = write_(STDOUT_FILENO, buf, 3);
+	if(w == -1)
+		perror(strerror(errno));
 	return 0;
 }
