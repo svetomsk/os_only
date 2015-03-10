e87ab9269a309ad717b710b0d23ebce9f19fd53d
svetomsk <svt2404@gmail.com>
2015-03-10 20:35:51 +0300

    write_ function implementation. Need to be tested as well

--- lib/helpers.c
+++ lib/helpers.c
@@ -17,13 +17,25 @@ ssize_t read_(int fd, void *buf, size_t count) {
 }
 
 ssize_t write_(int fd, void *buf, size_t count) {
-	return 0;
+	size_t result = 0;
+	ssize_t cur_writen;
+
+	while(count > 0 && (cur_writen = write(fd, buf + result, count)) > 0) {
+		result += cur_writen;
+		count -= cur_writen;
+	}
+
+	if(cur_writen == -1) {
+		return -1;
+	}
+
+	return result;
 }
 
 int main() {
 	char buf[5];
-	ssize_t r = read_(STDIN_FILENO, buf, 0);
+	ssize_t r = read_(STDIN_FILENO, buf, 5);
 	printf("%d\n", r);
-	write(STDOUT_FILENO, buf, 5);
+	ssize_t w = write_(STDOUT_FILENO, buf, r);
 	return 0;
 }
