d9a9e3b2b8a884d6f959f2b002c9e1792437a747
svetomsk <svt2404@gmail.com>
2015-03-10 20:27:09 +0300

    read_ function implementation. Need to be tested.

--- lib/helpers.c
+++ lib/helpers.c
@@ -1,7 +1,19 @@
 #include "helpers.h"
 
 ssize_t read_(int fd, void *buf, size_t count) {
-	return 0;
+	size_t result = 0;
+	ssize_t cur_read;
+
+	while(count > 0 && (cur_read = read(fd, buf + result, count)) > 0) {
+		result += cur_read;
+		count -= cur_read;
+	}
+
+	if(!feof(STDIN_FILENO) && count > 0) {
+		return -1;
+	}
+
+	return result;
 }
 
 ssize_t write_(int fd, void *buf, size_t count) {
@@ -9,5 +21,9 @@ ssize_t write_(int fd, void *buf, size_t count) {
 }
 
 int main() {
+	char buf[5];
+	ssize_t r = read_(STDIN_FILENO, buf, 0);
+	printf("%d\n", r);
+	write(STDOUT_FILENO, buf, 5);
 	return 0;
 }
