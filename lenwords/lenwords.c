e6da5eeaa6e32ce647e049eb220622ac5408c63f
svetomsk <svt2404@gmail.com>
2015-03-23 18:19:16 +0300

    Spaces fixed

--- lenwords/lenwords.c
+++ lenwords/lenwords.c
@@ -5,17 +5,15 @@ int main() {
 	char buf[4096];
 	ssize_t cur_read;
 	char convert[20];
-	int offset = 0;
-	while((cur_read = read_until(STDIN_FILENO, buf + offset, 4096, ' ')) > 0) {
+	while((cur_read = read_until(STDIN_FILENO, buf, 4096, ' ')) > 0) {
 		int r = cur_read;
-		if(buf[cur_read - 1] == ' ') {
-			r--;
-			offset = 0;
+		if(cur_read == 1 && buf[0] == ' ') {
+			sprintf(convert, " \n");
 		} else {
-			offset += cur_read;
+			if(buf[cur_read - 1] == ' ')
+				r--;
+			sprintf(convert, "%d\n", r);
 		}
-
-		sprintf(convert, "%d\n", r);
 		write_(STDOUT_FILENO, convert, 20);
 	}
 	return 0;
