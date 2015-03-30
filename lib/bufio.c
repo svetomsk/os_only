552f88c656a03446b713c56b814826d1ed6ac348
svetomsk <svt2404@gmail.com>
2015-03-30 20:40:34 +0300

    Basic functions for bufio. Fixed header from previous homework. It was forgotten.

---
+++ lib/bufio.c
@@ -0,0 +1,1 @@
+#include "bufio.h"
---
+++ lib/bufio.h
@@ -0,0 +1,20 @@
+#ifndef BUFIO_H
+#define BUFIO_H
+#include <stdlib.h>
+
+#define fd_t int
+
+
+struct buf_t {
+	size_t capacity;
+	size_t size;
+	char *buf;
+};
+
+struct buf_t *buf_new(size_t capacity);
+void buf_free(struct buf_t *);
+size_t buf_capacity(struct buf_t *);
+size_t buf_size(struct buf_t *);
+ssize_t buf_fill(fd_t fd,struct buf_t *buf, size_t required);
+
+#endif // BUFIO_H
