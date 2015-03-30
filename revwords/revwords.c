c1cce0cb4953c71f6a3a93cdd2fd21872f130167
svetomsk <svt2404@gmail.com>
2015-03-30 19:42:44 +0300

    Changes while defence commited

--- revwords/revwords.c
+++ revwords/revwords.c
@@ -1,5 +1,11 @@
 #include <helpers.h>
 #include <stdio.h>
+#include <errno.h>
+
+void print_error() {
+	printf("Error happend: %i", errno);
+	_exit(1);
+}
 
 char * revbuffer(char * buf, ssize_t length) {
 	size_t i = 0;
@@ -15,9 +21,15 @@ char * revbuffer(char * buf, ssize_t length) {
 int main() {
 	ssize_t cur_read;
 	char buf[4096];
-	while((cur_read = read_until(STDIN_FILENO, buf, 10, ' '))) {
-	 	write_(STDOUT_FILENO, revbuffer(buf, cur_read - 1), cur_read);
-	 	write_(STDOUT_FILENO, " ", 1);
+	while((cur_read = read_until(STDIN_FILENO, buf, 10, ' ') > 0) {
+		cur_read = read_until(STDIN_FILENO, buf, 10, ' ');
+	 	ssize_t written = write_(STDOUT_FILENO, revbuffer(buf, cur_read - 1), cur_read);
+	 	if(written == -1) {
+	 		print_error();
+	 	}
+	}
+	if(cur_read == -1) {
+		print_error();
 	}
 	return 0;
 }
