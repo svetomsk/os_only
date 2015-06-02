a9427f37d4a375cdd0169c9f0cd69ea39fc202d5
svetomsk <svt2404@gmail.com>
2015-06-02 14:38:46 +0300

    Some bugs in bufio fixed.

--- lib/bufio.c
+++ lib/bufio.c
@@ -26,7 +26,7 @@ size_t buf_size(struct buf_t * b) {
 ssize_t buf_fill(fd_t fd, struct buf_t *buf, size_t required) {
 	ssize_t cur_read;
 	size_t total_result = 0;
-	while((cur_read = read_(fd, buf->buf + total_result, buf->capacity)) > 0) {
+	while((cur_read = read(fd, buf->buf + total_result, buf->capacity)) > 0 && total_result < required) {
 		total_result += cur_read;
 	}
 
@@ -41,7 +41,7 @@ ssize_t buf_fill(fd_t fd, struct buf_t *buf, size_t required) {
 ssize_t buf_flush(fd_t fd, struct buf_t *buf, size_t required) {
 	ssize_t cur_written;
 	size_t total_result = 0;
-	while((cur_written = write_(fd, buf->buf, buf->size)) > 0) {
+	while((cur_written = write(fd, buf->buf, buf->size)) > 0) {
 		total_result += cur_written;
 		buf->size -= cur_written;
 	}
@@ -57,8 +57,10 @@ ssize_t buf_flush(fd_t fd, struct buf_t *buf, size_t required) {
 ssize_t buf_getline(fd_t fd, struct buf_t* buf, char* dest) {
 	size_t cur_pos = 0;
 	for(size_t i = 0; i < buf->size; i++, cur_pos++) {
-		if(buf->buf[i] == '\n')
+		if(buf->buf[i] == '\n') {
+			dest[cur_pos] = 0;
 			return i;
+		}
 		dest[i] = buf->buf[i];
 	}
 	buf_free(buf);
@@ -68,6 +70,7 @@ ssize_t buf_getline(fd_t fd, struct buf_t* buf, char* dest) {
 	while((read = buf_fill(fd, buf, 1)) > 0) {
 		for(size_t i = 0; i < buf->size; i++, cur_pos++) {
 			if(buf->buf[i] == '\n') {
+				dest[cur_pos] = 0;
 				return cur_pos;
 			}				
 			dest[cur_pos] = buf->buf[i];
